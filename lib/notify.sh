47df1ab0d8ed962c9248256b213c4715908f6900
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-08-28 00:20:22 +0000

    fix rm file when flameshot is aborted

--- lib/notify.sh
+++ lib/notify.sh
@@ -1,11 +1,16 @@
 notify() {
-  latest_file=`/bin/ls -th ${screenshot_path} | head -n 1`
+  if [[ ! -d ${path_backup} ]]; then
+    latest_file=$(/bin/ls -th ${screenshot_path} | sed -n 1p)
+  else
+    latest_file=$(/bin/ls -th ${screenshot_path} | sed -n 2p)
+  fi
   size=`find ${screenshot_path}/${latest_file} -printf %s`
 
   if (( ${size} == 0 || ${size} <= 20 )); then
     rm -rf "${screenshot_path}/${latest_file}"
+    rm -rf "${path_backup}/${latest_file}"
     notify-send "Awesomeshot" "Improving failed!" -t 3000
     exit 1
   fi
-  notify-send "Awesomeshot" "Improving success!" -t 3000
+  notify-send -i ${full_file_name} "Awesomeshot" "Improving success!" -t 3000
 }
